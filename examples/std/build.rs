fbc38074f2eac17456556dc9e1fc11482ef71f1d
Kaspar Schleiser <kaspar@schleiser.de>
2025-03-29 23:21:48 +0100

    feat: add std example

---
+++ examples/std/build.rs
@@ -0,0 +1,11 @@
+use std::env;
+
+fn main() {
+    // add linker script for embedded-test!!
+    println!("cargo::rustc-link-arg-tests=../../embedded-test.x");
+
+    // Check if the `defmt` feature is enabled, and if so link its linker script
+    if env::var("CARGO_FEATURE_DEFMT").is_ok() {
+        panic!("defmt not supported on std!");
+    }
+}
---
+++ examples/std/tests/example_test.rs
@@ -0,0 +1,78 @@
+#![no_main]
+
+/// Sets up the logging before entering the test-body, so that embedded-test internal logs (e.g. Running Test <...>)  can also be printed.
+/// Note: you can also inline this method in the attribute. e.g. `#[embedded_test::tests(setup=rtt_target::rtt_init_log!())]`
+fn setup_log() {
+    println!("setup_log()");
+}
+
+#[cfg(test)]
+#[embedded_test::tests(setup=crate::setup_log())]
+mod unit_tests {
+    // Optional: A init function which is called before every test
+    // async + return value are optional
+    #[init]
+    async fn init() -> u32 {
+        1234
+    }
+
+    // A test which takes the state returned by the init function (optional)
+    // asyncness is optional and needs feature embassy
+    #[test]
+    async fn takes_state(_state: u32) {
+        assert!(true)
+    }
+
+    // Example for a test which is conditionally enabled
+    #[test]
+    #[cfg(feature = "log")]
+    fn log() {
+        log::info!("Hello, log!");
+        assert!(true)
+    }
+
+    // A test which is cfg'ed out
+    #[test]
+    #[cfg(abc)]
+    fn it_works_disabled() {
+        assert!(false)
+    }
+
+    // Tests can be ignored with the #[ignore] attribute
+    #[test]
+    #[ignore]
+    fn it_works_ignored() {
+        assert!(false)
+    }
+
+    // A test that fails with a panic
+    #[test]
+    fn it_fails1() {
+        assert!(false)
+    }
+
+    // A test that fails with a returned Err(&str)
+    #[test]
+    fn it_fails2() -> Result<(), &'static str> {
+        Err("It failed because ...")
+    }
+
+    // Tests can be annotated with #[should_panic] if they are expected to panic
+    #[test]
+    #[should_panic]
+    fn it_passes() {
+        assert!(false)
+    }
+
+    // This test should panic, but doesn't => it fails
+    #[test]
+    #[should_panic]
+    fn it_fails3() {}
+
+    // Tests can be annotated with #[timeout(<secs>)] to change the default timeout of 60s
+    #[test]
+    #[timeout(2)]
+    fn it_timeouts() {
+        loop {} // should run into the 10s timeout
+    }
+}
