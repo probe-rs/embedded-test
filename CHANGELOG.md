56c1b366648ca5b37b8b2695f59b206ec4ab0f5f
Dániel Buga <bugadani@gmail.com>
2025-08-05 12:19:54 +0200

    Update embassy-executor

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+### Changed
+
+- Updated embassy-executor to 0.8
+
 ## [0.7.0-alpha.1]
 
 ### Added
--- src/export.rs
+++ src/export.rs
@@ -12,7 +12,7 @@ pub use embassy_executor::task;
     not(feature = "external-executor"),
     not(feature = "ariel-os")
 ))]
-pub use embassy_executor::Executor; // Please activate the `executor-thread` or `executor-interrupt` feature on the embassy-executor crate (v0.7.x)!
+pub use embassy_executor::Executor; // Please activate the `executor-thread` or `executor-interrupt` feature on the embassy-executor crate (v0.8.x)!
 
 pub fn check_outcome<T: TestOutcome>(outcome: T) -> ! {
     if outcome.is_success() {
