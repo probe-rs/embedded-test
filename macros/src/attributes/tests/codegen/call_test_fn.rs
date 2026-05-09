9f2adbd6fd0562f5c140b551ab1dc0a3a74f556e
Jakob Jungreuthmayer <116195279+jakobjung10@users.noreply.github.com>
2026-05-09 22:13:30 +0200

    rust-analyzer: ide support

--- macros/src/attributes/tests/codegen/call_test_fn.rs
+++ macros/src/attributes/tests/codegen/call_test_fn.rs
@@ -13,17 +13,22 @@ fn invoke(func: &ItemFn, args: Vec<TokenStream>) -> TokenStream {
 }
 
 /// Generate a code block ( in { ... }) to call the init function (if provided), call the test function and check the outcome.
-pub(crate) fn call_test_fn(test_func: &TestFunc, init_func: Option<&InitFunc>) -> TokenStream {
+pub(crate) fn call_test_fn(
+    test_func: &TestFunc,
+    func_ident: &syn::Ident,
+    init_func: Option<&InitFunc>,
+) -> TokenStream {
     let init_expr = if let Some(init) = init_func {
         invoke(&init.func, vec![])
     } else {
         quote!(())
     };
 
-    let run_call = if test_func.input.is_some() {
-        invoke(&test_func.func, vec![quote!(state)])
-    } else {
-        invoke(&test_func.func, vec![])
+    let run_call = match (test_func.input.is_some(), test_func.asyncness) {
+        (true, true) => quote!(#func_ident(state).await),
+        (true, false) => quote!(#func_ident(state)),
+        (false, true) => quote!(#func_ident().await),
+        (false, false) => quote!(#func_ident()),
     };
 
     quote!(
