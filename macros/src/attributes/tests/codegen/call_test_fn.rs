41fa60576e9be52e79bc5528e2e2bc4038bd05fc
Jakob Jungreuthmayer <116195279+jakobjung10@users.noreply.github.com>
2026-06-01 16:04:21 +0200

    rust-analyzer: ide support

--- macros/src/attributes/tests/codegen/call_test_fn.rs
+++ macros/src/attributes/tests/codegen/call_test_fn.rs
@@ -13,18 +13,23 @@ fn invoke(func: &ItemFn, args: Vec<TokenStream>) -> TokenStream {
 }
 
 /// Generate a code block ( in { ... }) to call the init function (if provided), call the test function and check the outcome.
-pub(crate) fn call_test_fn(test_func: &TestFunc, init_func: Option<&InitFunc>) -> TokenStream {
+pub(crate) fn call_test_fn(
+    test_func: &TestFunc,
+    body_fn: &ItemFn,
+    init_func: Option<&InitFunc>,
+) -> TokenStream {
     let init_expr = if let Some(init) = init_func {
         invoke(&init.func, vec![])
     } else {
         quote!(())
     };
 
-    let run_call = if test_func.input.is_some() {
-        invoke(&test_func.func, vec![quote!(state)])
+    let args = if test_func.input.is_some() {
+        vec![quote!(state)]
     } else {
-        invoke(&test_func.func, vec![])
+        vec![]
     };
+    let run_call = invoke(body_fn, args);
 
     quote!(
         {
